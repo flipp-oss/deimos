c842abfe3bc4c3485df8cb6f7b47d94d86d510fe
Daniel Orner <daniel.orner@wishabi.com>
2019-08-08 14:52:35 -0400

    Rubocop

--- spec/utils/db_producer_spec.rb
+++ spec/utils/db_producer_spec.rb
@@ -60,7 +60,7 @@ each_db_config(Deimos::Utils::DbProducer) do
     end
 
     it 'should split the batch size on buffer overflow' do
-      class_producer = double(Phobos::Producer::ClassMethods::PublicAPI,
+      class_producer = double(Phobos::Producer::ClassMethods::PublicAPI, # rubocop:disable RSpec/VerifiedDoubles
                               sync_producer_shutdown: nil)
       allow(producer.class).to receive(:producer).and_return(class_producer)
       expect(class_producer).to receive(:sync_producer_shutdown).twice
