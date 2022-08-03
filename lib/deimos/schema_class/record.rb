3df48344d2d187fbee851032755376002143c997
Daniel Orner <daniel.orner@wishabi.com>
2022-08-03 09:25:27 -0400

    This method is always overridden by the generator

--- lib/deimos/schema_class/record.rb
+++ lib/deimos/schema_class/record.rb
@@ -25,12 +25,6 @@ module Deimos
         obj
       end
 
-      # Converts the object to a hash which can be used for debugging or comparing objects.
-      # @return [Hash] a hash representation of the payload
-      def as_json(_opts={})
-        super.except('payload_key')
-      end
-
       # Element access method as if this Object were a hash
       # @param key[String||Symbol]
       # @return [Object] The value of the attribute if exists, nil otherwise
