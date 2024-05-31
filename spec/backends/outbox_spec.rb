190bd8d0b2d94639a9ecd378c8bbeefc21474e06
Daniel Orner <daniel.orner@flipp.com>
2024-05-31 14:41:42 -0400

    Producer and remaining specs

--- spec/backends/outbox_spec.rb
+++ spec/backends/outbox_spec.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
-each_db_config(Deimos::Backends::Db) do
+each_db_config(Deimos::Backends::Outbox) do
   include_context 'with publish_backend'
 
   it 'should save to the database' do
-    expect(Deimos.config.metrics).to receive(:increment).with(
-      'db_producer.insert',
-      tags: %w(topic:my-topic),
-      by: 3
-    )
+    # expect(Deimos.config.metrics).to receive(:increment).with(
+    #   'outbox.insert',
+    #   tags: %w(topic:my-topic),
+    #   by: 3
+    # )
     described_class.publish(producer_class: MyProducer, messages: messages)
     records = Deimos::KafkaMessage.all
     expect(records.size).to eq(3)
     expect(records[0].attributes.to_h).to include(
-      'message' => '{"foo"=>1}',
+      'message' => '{"test_id":"foo1","some_int":1}',
       'topic' => 'my-topic',
-      'key' => 'foo1'
+      'key' => '{"test_id":"foo1"}'
     )
     expect(records[1].attributes.to_h).to include(
-      'message' => '{"foo"=>2}',
+      'message' => '{"test_id":"foo2","some_int":2}',
       'topic' => 'my-topic',
-      'key' => 'foo2'
+      'key' => '{"test_id":"foo2"}'
     )
     expect(records[2].attributes.to_h).to include(
-      'message' => '{"foo"=>3}',
+      'message' => '{"test_id":"foo3","some_int":3}',
       'topic' => 'my-topic',
-      'key' => 'foo3'
+      'key' => '{"test_id":"foo3"}'
     )
   end
 
@@ -37,18 +37,12 @@ each_db_config(Deimos::Backends::Db) do
   end
 
   it 'should add to non-keyed messages' do
+    orig_messages = messages.deep_dup
     described_class.publish(producer_class: MyNoKeyProducer,
                             messages: messages)
     expect(Deimos::KafkaMessage.count).to eq(3)
     described_class.publish(producer_class: MyNoKeyProducer,
-                            messages: [messages.first])
+                            messages: [orig_messages.first])
     expect(Deimos::KafkaMessage.count).to eq(4)
-  end
-
-  it 'should add messages with Hash keys with JSON encoding' do
-    described_class.publish(producer_class: MyProducer,
-                            messages: [build_message({ foo: 0 }, 'my-topic', { 'test_id' => 0 })])
-    expect(Deimos::KafkaMessage.count).to eq(1)
-    expect(Deimos::KafkaMessage.last.partition_key).to eq(%(---\ntest_id: 0\n))
   end
 end
